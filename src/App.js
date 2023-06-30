14142773b8ddae3b1e20a360fe3f98236cdb2c8d
Elior Abramoviz <elior@fb.com>
2023-06-30 04:45:56 +0300

    Add links

--- src/App.js
+++ src/App.js
@@ -8,11 +8,41 @@ function App() {
         <div className='profile-image'></div>
         <div className='logo'></div>
         <div className='links-container'>
-          <div className='link youtube'></div>
-          <div className='link instagram'></div>
-          <div className='link spotify'></div>
-          <div className='link apple-music'></div>
-          <div className='link tiktok'></div>
+          <a
+            target='_blank'
+            href='https://www.youtube.com/@-asafy8256'
+            rel='noreferrer'
+          >
+            <div className='link youtube'></div>
+          </a>
+          <a
+            target='_blank'
+            href='https://www.instagram.com/asaf_abramoviz/'
+            rel='noreferrer'
+          >
+            <div className='link instagram'></div>
+          </a>
+          <a
+            target='_blank'
+            href='https://open.spotify.com/artist/0N6nQ7oHqLhCxAMvPo7ww1?si=ERbA_s4ZTauOGlPPF4vajQ'
+            rel='noreferrer'
+          >
+            <div className='link spotify'></div>
+          </a>
+          <a
+            target='_blank'
+            href='https://music.apple.com/il/artist/%D7%90%D7%A1%D7%A4%D7%99/1506017989?l=iw'
+            rel='noreferrer'
+          >
+            <div className='link apple-music'></div>
+          </a>
+          <a
+            target='_blank'
+            href='https://www.tiktok.com/@asafabramoviz1'
+            rel='noreferrer'
+          >
+            <div className='link tiktok'></div>
+          </a>
         </div>
       </div>
     </div>
